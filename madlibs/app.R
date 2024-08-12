5c59a901f3131e24833353f7878399e86edcc0b7
erinruth <erinruthbutler@gmail.com>
2024-08-12 11:31:00 -0700

    x

---
+++ madlibs/app.R
@@ -0,0 +1,48 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+generate_story <- function(noun, verb, adjective, adverb) {
+  glue::glue("
+    Once upon a time, there was a {adjective} {noun} who loved to
+    {verb} {adverb}. It was the funniest thing ever!
+  ")
+}
+
+# Define UI for application
+ui <- fluidPage(
+  titlePanel("Mad Libs Game"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput("noun1", "Enter a noun:", ""),
+      textInput("verb", "Enter a verb:", ""),
+      textInput("adjective", "Enter an adjective:", ""),
+      textInput("adverb", "Enter an adverb:", ""),
+      actionButton("submit", "Create Story")
+    ),
+    mainPanel(
+      h3("Your Mad Libs Story:"),
+      textOutput("story")
+    )
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+  story <- eventReactive(input$submit, {
+    generate_story(input$noun1, input$verb, input$adjective, input$adverb)
+  })
+  output$story <- renderText({
+    story()
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
